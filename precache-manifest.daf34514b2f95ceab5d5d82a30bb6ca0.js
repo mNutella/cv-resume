a98890cae3ff10f2829e1114ad4509c5c0e27395
mNutella <netcore.click@gmail.com>
2019-09-02 12:01:38 +0500

    Updates

---
+++ precache-manifest.daf34514b2f95ceab5d5d82a30bb6ca0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff8df62e4d1dc00dbbf92f3c5a9cbc6",
+    "url": "/cv-resume/index.html"
+  },
+  {
+    "revision": "f6b9f619dca2888133f4",
+    "url": "/cv-resume/static/css/main.35b8672b.chunk.css"
+  },
+  {
+    "revision": "94f42b7a807125a2a46d",
+    "url": "/cv-resume/static/js/2.c0fc8f3b.chunk.js"
+  },
+  {
+    "revision": "f6b9f619dca2888133f4",
+    "url": "/cv-resume/static/js/main.0a4c1cf2.chunk.js"
+  },
+  {
+    "revision": "73f8ed390b98efe97edd",
+    "url": "/cv-resume/static/js/runtime~main.c97b4007.js"
+  },
+  {
+    "revision": "70504770b2f62b3e18357d9bba9d46d6",
+    "url": "/cv-resume/static/media/bg.70504770.jpg"
+  }
+]);
